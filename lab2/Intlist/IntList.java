3d3d696813c57f03debbcab38b46ae60a67d7159
q <2980577404@qq.com>
2020-08-15 21:27:10 +0800

    finished

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,13 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null && B ==null) return null;
+        IntList ptr = A;
+        while (ptr.rest != null){
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +97,22 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null && B == null) return null;
+        IntList res = new IntList(A.first, null);
+        IntList ptr = res;
+         A = A.rest;
+        IntList ptrB = B;
+        while(A != null){
+            ptr.rest = new IntList(A.first,null);
+            ptr = ptr.rest;
+            A = A.rest;
+        }
+        while(ptrB != null){
+            ptr.rest = new IntList(ptrB.first,null);
+            ptr = ptr.rest;
+            ptrB = ptrB.rest;
+        }
+        return res;
     }
 
 
